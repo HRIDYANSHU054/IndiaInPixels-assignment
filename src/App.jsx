a4c418232cf1b522ccdfb99f03b2e14b0beeccc1
Hridyanshu7 <himank7794@gmail.com>
2024-10-15 20:47:02 +0530

    some minor changes for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import useDailyTemp from "./hooks/useDailyTemp";
 const API_KEY = import.meta.env.VITE_WEATHER_API;
 const API_BASE = `https://www.meteosource.com/api/v1/free/point?sections=current%2Cdaily&language=en&units=auto&key=${API_KEY}&`;
 // lat=${lat}&lon=${lng}
-console.log(API_KEY);
 function App() {
   const { data: dailyTemp, isLoading } = useDailyTemp(API_BASE, stateCoords);
 
